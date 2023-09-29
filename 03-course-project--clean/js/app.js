15eaab294125ad7bd072238d7fd0ac9f6f6d815f
Rob Pataki <robert@robertpataki.com>
2023-09-29 17:24:36 +0100

    Delete final project and use clean project to finish the tutorial

--- 03-course-project--clean/js/app.js
+++ 03-course-project--clean/js/app.js
@@ -10,6 +10,8 @@ import fragment from "./shaders/fragment.glsl";
 import vertex from "./shaders/vertex.glsl";
 import noise from "./shaders/noise.glsl";
 
+import Scroll from "./scroll.js";
+
 import ocean from "../img/ocean.jpg";
 
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
@@ -72,7 +74,6 @@ export default class Sketch {
 
     let allDone = [fontOpen, fontPlayfair, preloadImages];
     this.currentScroll = 0;
-    this.previousScroll = 0;
     this.raycaster = new THREE.Raycaster();
     this.mouse = new THREE.Vector2();
 
@@ -85,86 +86,25 @@ export default class Sketch {
       this.resize();
       this.setupResize();
       // this.addObjects();
-      this.composerPass();
       this.render();
-      // window.addEventListener('scroll',()=>{
-      //     this.currentScroll = window.scrollY;
-      //     this.setPosition();
-      // })
     });
   }
 
-  composerPass() {
-    this.composer = new EffectComposer(this.renderer);
-    this.renderPass = new RenderPass(this.scene, this.camera);
-    this.composer.addPass(this.renderPass);
+  mouseMovement(event) {
+    window.addEventListener("mousemove", (event) => {
+      this.mouse.x = (event.clientX / this.width) * 2 - 1;
+      this.mouse.y = -(event.clientY / this.height) * 2 + 1;
 
-    //custom shader pass
-    var counter = 0.0;
-    this.myEffect = {
-      uniforms: {
-        tDiffuse: { value: null },
-        scrollSpeed: { value: null },
-        time: { value: null },
-      },
-      vertexShader: `
-        varying vec2 vUv;
-        void main() {
-          vUv = uv;
-          gl_Position = projectionMatrix 
-            * modelViewMatrix 
-            * vec4( position, 1.0 );
-        }
-        `,
-      fragmentShader: `
-        uniform sampler2D tDiffuse;
-        varying vec2 vUv;
-        uniform float scrollSpeed;
-        uniform float time;
-        ${noise}
-        void main(){
-          vec2 newUV = vUv;
-          float area = smoothstep(1.,0.8,vUv.y)*2. - 1.;
-          float area1 = smoothstep(0.4,0.0,vUv.y);
-          area1 = pow(area1,4.);
-          float noise = 0.5*(cnoise(vec3(vUv*10.,time/5.)) + 1.);
-          float n = smoothstep(0.5,0.51, noise + area/2.);
-          newUV.x -= (vUv.x - 0.5)*0.1*area1*scrollSpeed;
-          gl_FragColor = texture2D( tDiffuse, newUV);
-        //   gl_FragColor = vec4(n,0.,0.,1.);
-        gl_FragColor = mix(vec4(1.),texture2D( tDiffuse, newUV),n);
-        // gl_FragColor = vec4(area,0.,0.,1.);
-        }
-        `,
-    };
+      // update the picking ray with the camera and pointer position
+      this.raycaster.setFromCamera(this.mouse, this.camera);
 
-    this.customPass = new ShaderPass(this.myEffect);
-    this.customPass.renderToScreen = true;
+      // calculate objects intersecting the picking ray
+      const intersects = this.raycaster.intersectObjects(this.scene.children);
 
-    this.composer.addPass(this.customPass);
-  }
-
-  mouseMovement() {
-    window.addEventListener(
-      "mousemove",
-      (event) => {
-        this.mouse.x = (event.clientX / this.width) * 2 - 1;
-        this.mouse.y = -(event.clientY / this.height) * 2 + 1;
-
-        // update the picking ray with the camera and mouse position
-        this.raycaster.setFromCamera(this.mouse, this.camera);
-
-        // calculate objects intersecting the picking ray
-        const intersects = this.raycaster.intersectObjects(this.scene.children);
-
-        if (intersects.length > 0) {
-          // console.log(intersects[0]);
-          let obj = intersects[0].object;
-          obj.material.uniforms.hover.value = intersects[0].uv;
-        }
-      },
-      false
-    );
+      if (intersects.length > 0) {
+        console.log(intersects[0]);
+      }
+    });
   }
 
   setupResize() {
@@ -185,16 +125,13 @@ export default class Sketch {
         time: { value: 0 },
         uImage: { value: 0 },
         hover: { value: new THREE.Vector2(0.5, 0.5) },
-        hoverState: { value: 0 },
         oceanTexture: { value: new THREE.TextureLoader().load(ocean) },
       },
       side: THREE.DoubleSide,
-      fragmentShader: fragment,
+      // fragmentShader: fragment,
       vertexShader: vertex,
-      // wireframe: true
+      wireframe: true,
     });
-
-    this.materials = [];
 
     this.imageStore = this.images.map((img) => {
       let bounds = img.getBoundingClientRect();
@@ -207,31 +144,11 @@ export default class Sketch {
       );
       let texture = new THREE.Texture(img);
       texture.needsUpdate = true;
-      // let material = new THREE.MeshBasicMaterial({
-      //     // color: 0xff0000,
-      //     map: texture
-      // })
-
       let material = this.material.clone();
-
-      img.addEventListener("mouseenter", () => {
-        gsap.to(material.uniforms.hoverState, {
-          duration: 1,
-          value: 1,
-          ease: "power3.out",
-        });
-      });
-      img.addEventListener("mouseout", () => {
-        gsap.to(material.uniforms.hoverState, {
-          duration: 1,
-          value: 0,
-          ease: "power3.out",
-        });
-      });
-
-      this.materials.push(material);
-
-      material.uniforms.uImage.value = texture;
+      /* let material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        map: texture,
+      }); */
 
       let mesh = new THREE.Mesh(geometry, material);
 
@@ -246,8 +163,6 @@ export default class Sketch {
         height: bounds.height,
       };
     });
-
-    console.log(this.imageStore);
   }
 
   setPosition() {
@@ -259,8 +174,8 @@ export default class Sketch {
   }
 
   addObjects() {
-    this.geometry = new THREE.PlaneBufferGeometry(200, 400, 10, 10);
-    // this.geometry = new THREE.SphereBufferGeometry( 0.4, 40,40 );
+    this.geometry = new THREE.PlaneGeometry(200, 400, 10, 10);
+    // this.geometry = new THREE.SphereGeometry(0.4, 40, 40);
     this.material = new THREE.MeshNormalMaterial();
 
     this.material = new THREE.ShaderMaterial({
@@ -282,25 +197,10 @@ export default class Sketch {
     this.time += 0.05;
 
     this.scroll.render();
-    this.previousScroll = this.currentScroll;
-
     this.currentScroll = this.scroll.scrollToRender;
 
-    // if(Math.round(this.currentScroll)!==Math.round(this.previousScroll)){
-    // console.log('should render');
     this.setPosition();
-    this.customPass.uniforms.scrollSpeed.value = this.scroll.speedTarget;
-    this.customPass.uniforms.time.value = this.time;
-
-    // this.material.uniforms.time.value = this.time;
-
-    this.materials.forEach((m) => {
-      m.uniforms.time.value = this.time;
-    });
-
-    // this.renderer.render( this.scene, this.camera );
-    this.composer.render();
-    // }
+    this.renderer.render(this.scene, this.camera);
 
     window.requestAnimationFrame(this.render.bind(this));
   }
